341d91679aab12553b0a2cf2460f2f6839df5637
Neckster1233 <143205804+Neckster1233@users.noreply.github.com>
2023-09-11 16:43:58 -0600

    Lab1

---
+++ Lab1.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "John";
+      Console.WriteLine(name);  
+      int myNum	= 6;
+      int myNum2 = 18;
+      int myNum3 = 21;
+      int Ans = (18/6);
+      Console.WriteLine(Ans);
+	  char myLetter = 'A';
+      string myWord = "boy";
+      Console.WriteLine(myLetter + " " + myWord);
+      float x = (6.3F);
+      Console.WriteLine(x);
+      string greeting = "Hello, how is it going?";
+      Console.WriteLine(greeting);
+      string greeting2 = "I'm great! How are you?";
+      string greeting3 = greeting + " " + greeting2;
+      Console.WriteLine(greeting3);
+     }
+  }
+}
