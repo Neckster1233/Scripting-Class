2b3e2215e38317917627ee0ef652337cd0e9a998
Enoch Smith <enochsmith1233@gmail.com>
2023-09-27 17:31:44 -0600

    Unit 3 Lab

---
+++ Unit_1/My project (1)/Assets/Scenes/ContolingAGameObject.cs
@@ -0,0 +1,5 @@
+//Use this for initialization
+void start ()
+{
+Debug.Log("I am alive!");
+}
--- Unit_1/My project (1)/Assets/Scenes/PlayerController.cs
+++ Unit_1/My project (1)/Assets/Scenes/PlayerController.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[SerializeField] private float moveSpeed = 5f;
+[SerializeField] private float jumpForce = 5f;
+[SerializeField] private float gravity = 9.81f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+private CharacterController controller;
+private Vector3 moveDirection;
+private bool isJumping;
+
+void start()
+{
+    GetComponent<CharacterController>()
 }
+
+void Update()
+{
+    Input.GetAxis("Horizontal")
+    Input.GetAxis("Veritcal")
+    private float horizontalInput = 5f;
+    private float verticalInput = 5f;
+// Not sure if this part is correct
+    horizontalInput * moveSpeed = speedOfMovement
+    Input.GetButtonDown("Jump")
+    CharacterController.isGrounded
+    // Need to check if player is pressing jump here
+    moveDirection.y - (gravity * Time.deltaTime)
+    CharacterController.Move(moveDirection * Time.deltaTime)
+    //if jumping here
+    if isJumping = true
+        moveDirection.y = jumpForce
+}
+
