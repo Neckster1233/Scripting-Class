5e11b2bae19cc4101e4407ad269a1bef5b7065ac
Enoch Smith <enochsmith1233@gmail.com>
2023-10-02 16:34:57 -0600

    Deleted Player Controll Test

--- Unit_1/My project (1)/Assets/Scenes/PlayerControllerTest.cs
+++ Unit_1/My project (1)/Assets/Scenes/PlayerControllerTest.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerControllerTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float gravity = 9.81f;
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private bool isJumping = false;
+    private float horizontalInput = 5f;
+    private float verticalInput = 5f;
     void Start()
     {
-        
+        GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Input.GetAxis("Horizontal");
+        Input.GetAxis("Veritcal");
+        horizontalInput* moveSpeed = speedOfMovement;
+        Input.GetButtonDown("Jump");
+        isJumping = true;
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            isJumping = true;
+        }
+
+        moveDirection.y - (gravity * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+        //if jumping here
+        if (isJumping = true)
+        {
+            moveDirection.y = jumpForce;
+            isJumping = false;
+        }
+
     }
+
 }
