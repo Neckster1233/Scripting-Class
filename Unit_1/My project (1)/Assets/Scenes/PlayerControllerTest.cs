b8992b4541abc594418f51892a64cde7ca27812b
Enoch Smith <enochsmith1233@gmail.com>
2023-10-03 16:50:37 -0600

    Challenge 3

--- Unit_1/My project (1)/Assets/Scenes/PlayerControllerTest.cs
+++ Unit_1/My project (1)/Assets/Scenes/PlayerControllerTest.cs
@@ -7,12 +7,15 @@ public class PlayerControllerTest : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float gravity = 9.81f;
-
+	[SerializeField] private float height = 6.3f;
     private CharacterController controller;
     private Vector3 moveDirection;
     private bool isJumping = false;
     private float horizontalInput = 5f;
     private float verticalInput = 5f;
+	private bool isCrouching = false;
+	private bool isSpringting = false;
+	private bool isRotating = false;
     void Start()
     {
         GetComponent<CharacterController>();
@@ -22,7 +25,7 @@ public class PlayerControllerTest : MonoBehaviour
     {
         Input.GetAxis("Horizontal");
         Input.GetAxis("Veritcal");
-        horizontalInput* moveSpeed = speedOfMovement;
+        horizontalInput * moveSpeed = speedOfMovement;
         Input.GetButtonDown("Jump");
         isJumping = true;
         if (Input.GetButtonDown("Jump") && controller.isGrounded)
@@ -38,7 +41,25 @@ public class PlayerControllerTest : MonoBehaviour
             moveDirection.y = jumpForce;
             isJumping = false;
         }
-
-    }
-
+		Input.GetButtonDown("Crouch");
+		if (Input.GetButtonDown("Crouch") && controller.isGrounded)
+			{
+			isCrouching = true;
+			}
+		if (isJumping = false && isCrouching = true)
+			{
+			private float height = 3.15f;
+			}
+		Input.GetButtonDown("Sprint");
+		if (isSprinting = true)
+			{
+			speedOfMovement * 2
+			}
+		Inpute.GetButtonDown("Rotate");
+		if (isRotating = true)
+			{
+			horizontalInput = "Rotate"
+			}
+		
+	}
 }
