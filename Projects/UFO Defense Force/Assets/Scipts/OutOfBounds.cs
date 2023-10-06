e207238553420aae3e431832be0dc12e2218a002
Enoch Smith <enochsmith1233@gmail.com>
2023-10-05 18:53:50 -0600

    Level ! Scripts + Game Objects

---
+++ Projects/UFO Defense Force/Assets/Scipts/OutOfBounds.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    public float topBounds = 5.0f;
+
+    public float bottomBounds = -10.0f;
+
+    void Awake()
+    {
+       // Time.timeScale = 1;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < bottomBounds)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+            // Time.timeScael = 0;
+        }
+    }
+}
---
+++ Projects/UFO Defense Force/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+
+    public float speed;
+
+    public float xRange;
+    public Transform blaster;
+    public GameObject laser;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Added horizontal axis
+        horizontalInput = Input.GetAxis("Horizontal");
+        // Moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        //Left Side Wall
+        if(transform.position.x < -xRange )
+        {
+            transform.position = new Vector3( -xRange, transform.position.y, transform.position.z);
+        }    
+        // Right Side Wall
+        if(transform.position.x > xRange )
+        {
+            transform.position = new Vector3( xRange, transform.position.y, transform.position.z);
+        }
+        // Create Laser at the Blaster point
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate( laser, blaster.transform.position, laser.transform.rotation);
+        }    
+    }
+    //Delete any object that hits the player
+    /* Doesn't work for some reason
+     testing out multiline comments
+    */
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
